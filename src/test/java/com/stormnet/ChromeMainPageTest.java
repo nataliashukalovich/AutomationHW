6f4c68055c703b5e477f8c329d89ac8fbe7e7376
natalia.shukalovich <natalia.shukalovich@helmes.com>
2022-04-27 11:54:32 +0400

    hw fixed // 27.04

--- src/test/java/com/stormnet/ChromeMainPageTest.java
+++ src/test/java/com/stormnet/ChromeMainPageTest.java
@@ -2,20 +2,23 @@ package com.stormnet;
 
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
 public class ChromeMainPageTest {
-    private final String mainPage = "https://www.onliner.by/";
-    private final String catalogPage = "https://catalog.onliner.by/";
+    private static final String mainPage = "https://www.onliner.by/";
+    private static final String CATALOG_XPATH = "//a[@href = \"https://catalog.onliner.by\" and @class = \"b-main-navigation__link\"]";
 
     @Test
     public void testOpenPage() {
         open(mainPage);
-        open(catalogPage);
+        $(byXpath(CATALOG_XPATH)).click();
         Selenide.closeWebDriver();
     }
 
@@ -25,7 +28,7 @@ public class ChromeMainPageTest {
         WebDriver driver = new ChromeDriver();
         driver.get(mainPage);
         System.out.println("Page title is: " + driver.getTitle());
-        driver.get(catalogPage);
+        driver.findElement(By.xpath(CATALOG_XPATH)).click();
         System.out.println("Page title is: " + driver.getTitle());
         driver.quit();
     }
--- src/test/java/com/stormnet/FFMainPageTest.java
+++ src/test/java/com/stormnet/FFMainPageTest.java
@@ -2,24 +2,25 @@ package com.stormnet;
 
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.title;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.*;
 
 public class FFMainPageTest {
-    private final String mainPage = "https://belavia.by/";
-    private final String newsPage = "https://belavia.by/novosti/";
+    private static final String mainPage = "https://belavia.by/";
+    private static final String NEWSPAGE_XPATH = "//a[text()=\"Все новости\"]";
 
     @Test
     public void testOpenPage() {
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\natalia.shukalovich\\test\\geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
         driver.get(mainPage);
-        driver.get(newsPage);
+        driver.findElement(By.xpath(NEWSPAGE_XPATH)).click();
         driver.quit();
     }
 
@@ -27,7 +28,7 @@ public class FFMainPageTest {
     public void testPrintTitle() {
         open(mainPage);
         System.out.println("Title of the page is: " + title());
-        open(newsPage);
+        $(byXpath(NEWSPAGE_XPATH)).click();
         System.out.println("Title of the page is: " + title());
         Selenide.closeWebDriver();
     }
